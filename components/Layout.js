5d5e0dd9da752c9ec96e8983663e2593e20de1cf
= <=>
2023-02-01 15:18:38 +0100

    added mobile css + header image

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,32 @@
 import styles from "../styles/layout.module.css";
+import Image from 'next/image';
+import Link from "next/link";
+import logo from "../public/EstraLabs1-1small.png";
 
 export default function Layout({ children }) {
     return (
         <div className={styles.layout}>
             <header className={styles.title}>
-                <h1>EstraLabs</h1>
+                <div className={styles.menubar}>
+                    <Link href="/">
+                        <Image
+                            src={logo}
+                            alt="EstraLabs logo"
+                            width={200}
+                            height={150}
+                        />
+                    </Link>
+                </div>
             </header>
                 <div className={styles.content}>
                     {children}
+                    <Image 
+                        src="https://inconspicuousbucket.s3.us-west-2.amazonaws.com/e2v2.gif" 
+                        height={62}
+                        width={162}
+                        alt="Estrogen" 
+                        className={styles.spinningEstrogen}
+                    />
                 </div>
             <footer className={styles.copyright}>
                 <span>(c) 2023 EstraLabs</span>
---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+    images: {
+      domains: ['https://inconspicuousbucket.s3.us-west-2.amazonaws.com', "inconspicuousbucket.s3.us-west-2.amazonaws.com"],
+    },
+  }
+  
